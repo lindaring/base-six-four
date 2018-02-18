c773476c596713d2d15a443d339f6ef035124367
Lindaring <lndsimelane@gmail.com>
2018-02-18 21:55:33 +0200

    Repo init

---
+++ src/main/java/com/lindaring/base/BaseSixFourApiApplication.java
@@ -0,0 +1,13 @@
+package com.lindaring.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseSixFourApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseSixFourApiApplication.class, args);
+    }
+
+}
