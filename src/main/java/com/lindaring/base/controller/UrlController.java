2ddaa275bbe411c4f378183180d488f0e466eacc
Lindaring <lndsimelane@gmail.com>
2019-11-13 08:11:18 +0200

    Added controller for url shortener

---
+++ src/main/java/com/lindaring/base/controller/UrlController.java
@@ -0,0 +1,12 @@
+package com.lindaring.base.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/base-six-four/v1/url")
+public class UrlController {
+
+}
